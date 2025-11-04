e6dcd92002aac143e5625fa361ee0a64238970ed
Josh <josh@hessjd.com>
2025-11-04 17:01:45 -0600

    updates

--- control.v
+++ control.v
@@ -1,4 +1,5 @@
 module control (
+	input clk, reset,
 	input [5:0] op,
 
 	output reg memread, memwrite, alusrca, memtoreg, iord, regwrite, regdest,
@@ -6,6 +7,19 @@ module control (
 	output reg [1:0] pcsource, alusrcb, aluop,
 	output reg [3:0] iwrite
 );
+
+	reg [3:0] state, nextstate;
+
+	initial begin
+		memread <= (state == 4'b0000 || state == 4'b0011) ? 1 : 0;
+		memwrite <= (state == 4'b0101) ? 1 : 0;
+		alusrca <= (state == 4'b0010 || state == 4'b0110 || state == 4'b1000) ? 1 : 0;
+		memtoreg <= (state == 4'b0100) ? 1 : 0;
+		iord <= (state == 4'b0011 || state == 4'b0101) ? 1 : 0;
+		regwrite <= (state == 4'b0100 || state == 4'b0111) ? 1 : 0;
+		regdest <= (state == 4'b0111) ? 1 : 0;
+	end
+	
 
 	
 
